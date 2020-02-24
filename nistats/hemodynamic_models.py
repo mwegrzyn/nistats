2942034d766f51576a539f4208d1076986518494
mwegrzyn <martin.wegrzyn@uni-bielefeld.de>
2020-02-24 12:16:11 +0100

    changed behavior of _sample_condition when multiple events have the same offset

--- nistats/hemodynamic_models.py
+++ nistats/hemodynamic_models.py
@@ -290,7 +290,9 @@ def _sample_condition(exp_condition, frame_times, oversampling=50,
         if t < (tmax - 1) and t == t_onset[i]:
             t_offset[i] += 1
 
-    regressor[t_offset] -= values
+    for t in t_offset:
+        regressor[t_offset] -= values
+
     regressor = np.cumsum(regressor)
 
     return regressor, hr_frame_times
